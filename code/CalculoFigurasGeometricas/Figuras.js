89a7f0e76286daa57ee69b6fa642f43ebfd72c81
Freddy Marin <freddymarin.jpg@gmail.com>
2022-03-04 12:09:39 -0600

    Comienzo del taller 1 - Calculo de figuras geometricas. Hasta este punto, solo cuenta con las furmulas para el perimetro y area de un cuadrado

---
+++ code/CalculoFigurasGeometricas/Figuras.js
@@ -0,0 +1,16 @@
+// CODIGO DEL CUADRADO 
+
+// Definimos las formulas para el calculo del perimetro y area de un cuadrado
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden " + ladoCuadrado + " centimetros" );
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es de " + perimetroCuadrado + " centimetros" );
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es de " + areaCuadrado + " centimetros cuadrados" );
+
+// CODIGO DEL TRIANGULO
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 8;
+console.log("Los lados del triangulo miden " + ladoTriangulo1 + " cm, ",  ladoTriangulo2 + " cm, ", baseTriangulo + " cm siendo estos el lado 1, lado 2 y la base respectivamente" );
