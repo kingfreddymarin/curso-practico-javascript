e576ae5db920e701b3a0ebe3ce86728065dfb5fb
Freddy Marin <freddymarin.jpg@gmail.com>
2022-03-06 17:12:04 -0600

    Edicion de detalles - anadicion de altura con console log

--- code/CalculoFigurasGeometricas/Figuras.js
+++ code/CalculoFigurasGeometricas/Figuras.js
@@ -23,6 +23,8 @@ const alturaTriangulo = 7.5;
 const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
 const areaTriangulo = ( baseTriangulo * alturaTriangulo ) / 2;
 console.log("Los lados del triangulo miden " + ladoTriangulo1 + " cm, ",  ladoTriangulo2 + " cm, ", baseTriangulo + " cm siendo estos el lado 1, lado 2 y la base respectivamente" );
-console.log("El perimetro del triangulo mide "+perimetroTriangulo+" cm y el area es de "+areaTriangulo+" cm")
+console.log("la altura del triangulo es "+alturaTriangulo+" cm");
+console.log("El perimetro del triangulo mide "+perimetroTriangulo+" cm y el area es de "+areaTriangulo+" cm Cuadrados")
 
 console.groupEnd();
+
