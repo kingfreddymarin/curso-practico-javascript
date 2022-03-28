a240ca8e85783737bd84eb5776e51a8dc462fdb1
Freddy Marin <freddymarin.jpg@gmail.com>
2022-03-28 11:54:42 -0600

    Calculating geometric figures (square and triangle) - Interaccion en HTML directamente con JavaScript

--- code/CalculoFigurasGeometricas/FigurasFunciones.js
+++ code/CalculoFigurasGeometricas/FigurasFunciones.js
@@ -1,5 +1,4 @@
 // CODIGO DEL CUADRADO 
-
 function perimetroCuadrado(lado){
     return lado * 4;
 }
@@ -8,16 +7,14 @@ function areaCuadrado(lado){
 }
 
 // CODIGO DEL TRIANGULO
-
 function perimetroTriangulo(lado1, lado2, base){
-    return (lado1 + lado2 + base);
+    return lado1 + lado2 + base;
 }
 function areaTriangulo(base, altura){
     return (base * altura)/2;
 }
 
 //Codigo del circulo
-
 const PI = Math.PI;
 
 function diametroCirculo (radio){
@@ -29,4 +26,60 @@ function perimetroCirculo (radio){
 }
 function areaCirculo (radio){
     return (radio * radio) * PI;
+}
+//Interaccion con HTML  
+//Square functions
+function calculatePerimeterSquare(){
+    const input = document.getElementById("inputSquare");
+    const value = input.value;
+
+    const perimeter = perimetroCuadrado(value);
+    alert(perimeter);
+}
+function calculateAreaSquare(){
+    const input = document.getElementById("inputSquare");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+//triangle functions
+function calculatePerimeterTriangle(){
+    const left = document.getElementById("left");
+    const right = document.getElementById("right");
+    const base = document.getElementById("base");
+    const a = Number.parseFloat(left.value);
+    const b = Number.parseFloat(right.value);
+    const c = Number.parseFloat(base.value)
+
+    const perimeter = perimetroTriangulo(a, b, c);
+    alert(perimeter);
+}
+function calculateAreaTriangle(){
+    const left = document.getElementById("left");
+    const right = document.getElementById("right");
+    const base = document.getElementById("base");
+    const a = Number.parseFloat(left.value);
+    const b = Number.parseFloat(right.value);
+    const c = Number.parseFloat(base.value)
+    
+    if (a != b){
+        alert("No es un triangulo isoceles");
+    }else{
+        //spliting triangle in half
+        let miniA;
+        const miniB = c/2;
+        const miniC = b;
+    
+        //Turning values into square
+        const sqMiniB = miniB * miniB;
+        const sqMiniC = miniC * miniC;
+        const sqMiniA = sqMiniC - sqMiniB;
+        miniA = Math.sqrt(sqMiniA);
+    
+        //Height equals 
+        const height = miniA;
+        const area = areaTriangulo(c, height);
+        alert(area);
+    }
 }
